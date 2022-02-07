393440bb979b2b91190efca478e081f55cf906d8
ElderSam <eldersamuel98@gmail.com>
2022-02-07 08:41:34 -0300

    feat: create app (server logic)

---
+++ Users/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+// const routes = require("./routes");
+
+class AppController {
+	constructor() {
+		this.express = express();
+
+		this.middlewares();
+		this.routes();
+	}
+
+	middlewares() {
+		this.express.use(express.json());
+	}
+
+	routes() {
+		// this.express.use(routes)
+	}
+}
+
+module.exports = new AppController().express;
